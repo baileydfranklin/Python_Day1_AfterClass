2c6c314072301d74957c0a7f933d272f39c6e1d7
Bailey Franklin <baileydfranklin@gmail.com>
2023-07-02 12:09:15 -0700

    Pushing what I have to work on it later

---
+++ python.py
@@ -0,0 +1,17 @@
+print("Hello from inside a file!")
+
+def hello():
+    print("Hello user!")
+
+hello()
+
+def pack(x,y,z):
+    list (x,y,z)
+    print(list)
+
+    return(list)
+
+pack('My','name','is Jarvis!')
+
+def eat_lunch():
+    
